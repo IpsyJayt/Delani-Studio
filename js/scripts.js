8d4aa99cd3b8a210c13f53319c9433ec62a57411
IpsyJayt <ipsyjayt@gmail.com>
2021-11-22 01:50:16 +0300

    add an alert function for received message

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,13 @@ $(document).ready(function(){
         $("img#prod-management").toggle();
         $(".product-management-text").toggle();
     });
+    
+})
+
+$(document).ready(function(){
+    $("button#btn").click(function(event){
+        var userName= $("input#inputName").val();
+        alert( userName + " we have received your message. Thank you for reaching out to us.")
+        event.preventDefault();
+    })
 })
