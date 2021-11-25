1d6c8874da5be086dfdbf928fc6c6d258a43e837
IpsyJayt <ipsyjayt@gmail.com>
2021-11-25 16:23:27 +0300

    add overlay

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,55 @@ $(document).ready(function(){
         event.preventDefault();
     })
 })
+
+$(document).ready(function() {
+    $("#work1").mouseenter(function() {
+        $("#work1text").show();
+    });
+    $("#work1").mouseleave(function() {
+       $("#work1text").hide();
+    }); 
+    $("#work2").mouseenter(function() {
+        $("#work2text").show();        
+    });
+    $("#work2").mouseleave(function() {
+        $("#work2text").hide();
+     }); 
+    $("#work3").mouseenter(function() {
+       $("#work3text").hide();
+    }); 
+    $("#work3").mouseleave(function() {
+        $("#work3text").hide();
+     }); 
+    $("#work4").mouseenter(function() {
+        $("#work4text").show();
+    });
+    $("#work4").mouseleave(function() {
+       $("#work4text").hide();
+    });
+    $("#work5").mouseenter(function() {
+        $("#work5text").show();
+    });
+    $("#work5").mouseleave(function() {
+       $("#work5text").hide();
+    });   
+    $("#work6").mouseenter(function() {
+        $("#work6text").show();
+    });
+    $("#work6").mouseleave(function() {
+       $("#work6text").hide();
+    });  
+    $("#work7").mouseenter(function() {
+        $("#work7text").show();
+    });
+    $("#work7").mouseleave(function() {
+       $("#work7text").hide();
+    }); 
+    $("#work8").mouseenter(function() {
+        $("#work8text").show();
+    });
+    $("#work8").mouseleave(function() {
+       $("#work8text").hide();
+    }); 
+});
+
